74eba739d86bcd0b36f3c565daef9671b954cde2
Denis-Valentin Albăstroiu <denisvalentin100@gmail.com>
2026-05-28 17:33:23 +0300

    Test IR SENSOR + buzzer

--- src/main.c
+++ src/main.c
@@ -1,23 +1,39 @@
+#include <stdint.h>
 #include "drivers/gpio/gpio.h"
 #include "utils/delay.h"
 #include "bsp/nano.h"
 
-int main(void) {
-// test de IR SENSOR
-    GPIO_Init(D7, GPIO_INPUT);     
-    GPIO_Init(D4, GPIO_OUTPUT);    
+void Buzzer_Beep(uint16_t duration_ms)
+{
+    for (uint16_t i = 0; i < duration_ms / 2; i++)
+    {
+        GPIO_Write(D7, GPIO_HIGH);
+        Delay(1);
 
-    GPIO_Write(D4, GPIO_LOW);     
+        GPIO_Write(D7, GPIO_LOW);
+        Delay(1);
+    }
+
+    GPIO_Write(D7, GPIO_LOW);
+}
+
+int main(void)
+{
+    GPIO_Init(D4, GPIO_INPUT);     
+    GPIO_Write(D4, GPIO_HIGH);    
+
+    GPIO_Init(D7, GPIO_OUTPUT);    
+    GPIO_Write(D7, GPIO_LOW);     
 
     while (1)
     {
-        if (GPIO_Read(D7) == 0)
+        if (GPIO_Read(D4) == 0)
         {
-            GPIO_Write(D4, GPIO_HIGH);   
+            Buzzer_Beep(300);
         }
         else
         {
-            GPIO_Write(D4, GPIO_LOW);    
+            GPIO_Write(D7, GPIO_LOW);
         }
 
         Delay(20);
